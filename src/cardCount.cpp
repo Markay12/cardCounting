26f0e50144d4453b072f70b48654f7595f515b55
Markay12 <54555076+Markay12@users.noreply.github.com>
2020-12-28 17:24:01 -0700

    main source file and script to run

---
+++ src/cardCount.cpp
@@ -0,0 +1,37 @@
+// Mark Ashinhust
+// Card Couting and Data Distribution
+// November 2020
+
+
+#include<iostream>
+#include "Cards.h"
+#include "BagNode.h"
+
+using std::cin;
+using std::cout;
+
+
+
+int main()
+{
+
+
+	BagNode* deck1 = new BagNode();
+	deck1->addRandom();
+
+	cout << "\nThe first value is --> " << deck1->getCurrVal() << " of " << deck1->getCurrSuit() << "\n";
+
+
+	deck1->empty();
+
+
+	cout << "\nThe first value is --> " << deck1->getCurrVal() << " of " << deck1->getCurrSuit() << "\n";
+
+	deck1->fill();
+
+
+	cout << "\nThe first value is --> " << deck1->getCurrVal() << " of " << deck1->getCurrSuit() << "\n";
+
+	return 0;
+
+}
